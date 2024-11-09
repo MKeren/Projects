6617550fcf715d2b591290a52794c37762d08141
MKeren <166819106+MKeren@users.noreply.github.com>
2024-11-09 21:35:28 +0200

    Update settings.py

--- cloud/MySpace/MySpace/settings.py
+++ cloud/MySpace/MySpace/settings.py
@@ -132,14 +132,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# AWS S3 settings
-AWS_ACCESS_KEY_ID = 'AKIATAVABIB3EQAA7FHL'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'kera.github.io'
-AWS_S3_REGION_NAME = 'us-east-1'  
-AWS_S3_SIGNATURE_VERSION = 's3v4'
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 AWS_S3_FILE_OVERWRITE = False
