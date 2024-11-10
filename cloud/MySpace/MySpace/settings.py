a51cd68da292933bbcef69b5c4318779302870d6
Mkeren <tuzolana.keren@final.edu.tr>
2024-11-10 18:45:54 +0200

    donc

--- cloud/MySpace/MySpace/settings.py
+++ cloud/MySpace/MySpace/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'uploads/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -135,15 +136,24 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # AWS S3 settings
 AWS_ACCESS_KEY_ID = 'AKIATAVABIB3EQAA7FHL'
 AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'kera.github.io'
+AWS_STORAGE_BUCKET_NAME = 'drive-project-bucket'
 AWS_S3_REGION_NAME = 'us-east-1'  
 AWS_S3_SIGNATURE_VERSION = 's3v4'
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
+
+# Static files storage configuration
+STATIC_URL = f'https://drive-project-bucket.s3.amazonaws.com/static/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+# Media files storage configuration
+MEDIA_URL = f'https://drive-project-bucket.s3.amazonaws.com/uploads/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Local file paths (optional, used for local development)
+STATIC_ROOT = Path(__file__).resolve().parent / "staticfiles"
+MEDIA_ROOT = Path(__file__).resolve().parent / "mediafiles"
 
 
 LOGIN_URL = '/login/'
